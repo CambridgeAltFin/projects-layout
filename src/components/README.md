b50396a47758e5102aae90c994d1f324f9f5eeb6
bondarenko-ag <bondarenko_ag@groupbwt.com>
2024-06-13 08:12:48 +0300

    build: init ui lib

---
+++ src/components/README.md
@@ -0,0 +1,8 @@
+# All components imported by the library are stored here
+
+Approximate component structure :
+
+**/folder with the component name**
+**->  file-name.vue**  _Main component file_
+**->  file-name.type.ts** _File with all types and interfaces for the component_ 
+**->  file-name.stories.ts** _File for display in storybook, feature templates, etc._
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import vue from '@vitejs/plugin-vue';
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+import dts from 'vite-plugin-dts';
+import vuetify from 'vite-plugin-vuetify';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'projects-layout',
+      fileName: (format) => `projects-layout.${format}.ts`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    vuetify({autoImport: true, styles: 'sass'}),
+    dts({
+      insertTypesEntry: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+});
