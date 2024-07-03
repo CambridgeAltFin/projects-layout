dfcfbb6013b043e426591efb85457f55d3c09ca7
bondarenko-ag <bondarenko_ag@groupbwt.com>
2024-07-03 11:11:24 +0300

    fix: update versions and update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue2';
-import { resolve } from 'path';
+import {resolve} from 'path';
 import config from './package.json';
-import postcssImport from 'postcss-import';
-import postcss from 'postcss';
 
 export default defineConfig({
   plugins: [vue()],
@@ -12,31 +10,27 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.js'),
       name: config.name,
       formats: ['es', 'cjs', 'umd'],
-      fileName: (format) => `${config.name}.${format}.js`,
+      fileName: (format) => `${config.name}.${format}.js`
     },
     rollupOptions: {
-      external: ['vue', 'vuetify'],
+      external: ['vue'],
       output: {
         globals: {
-          vue: 'Vue',
-          vuetify: 'Vuetify',
-        },
-      },
-    },
+          vue: 'Vue'
+        }
+      }
+    }
   },
   css: {
-    postcss: {
-      plugins: [postcss(), postcssImport()],
-    },
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/styles/index.scss";`,
-      },
-    },
+        additionalData: `@import "@/assets/styles/index.scss";`
+      }
+    }
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+      '@': resolve(__dirname, 'src')
+    }
+  }
 });
