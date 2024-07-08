158cf26dc226ec4dbc88d9b5755b56360dd5fd8b
bondarenko-ag <bondarenko_ag@groupbwt.com>
2024-07-08 16:53:56 +0300

    fix: update build

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ import Vuetify from 'vuetify/lib';
 
 import 'vuetify/dist/vuetify.min.css';
 
+const opts = {
+  ssr: true,
+  components,
+  directives,
+  theme: {
+    themes: {
+      dark: false,
+      light: {
+        primary: '#0E1436',
+        accent: '#FFB71A',
+        secondary: '#828597'
+      }
+    }
+  }
+};
+
+Vue.use(new Vuetify(opts));
+
 import MobileMenu from './components/MobileMenu.vue';
 import LayoutFooter from './components/LayoutFooter.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
 import Loader from './components/Loader.vue';
 
 export {MobileMenu, LayoutFooter, LayoutHeader, Loader};
-
-export default () => {
-  Vue.use(Vuetify);
-  Vue.component('MobileMenu', MobileMenu);
-  Vue.component('LayoutFooter', LayoutFooter);
-  Vue.component('LayoutHeader', LayoutHeader);
-  Vue.component('Loader', Loader);
-};
